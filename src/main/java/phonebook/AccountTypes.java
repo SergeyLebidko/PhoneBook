d52b38b55ccba6ede0314be5203899bbdc38db9a
SergeyLebidko <it.sergeyler@mail.ru>
2019-04-29 13:35:09 +0300

    Добавлено перечисление типов аккаунтов

--- src/main/java/phonebook/AccountTypes.java
+++ src/main/java/phonebook/AccountTypes.java
@@ -2,6 +2,35 @@ package phonebook;
 
 public enum AccountTypes {
 
+    PHONE ("", "", "phone"),
+    FACEBOOK ("https://", "www.facebook.com/", "facebook"),
+    INSTAGRAM ("https://", "instagram.com/", "instagram"),
+    MAIL("mailto:", "", "mail"),
+    OK ("https://", "ok.ru/", "ok"),
+    TELEGRAM ("https://", "t.me/", "telegram"),
+    TWITTER ("https://", "twitter.com/", "twitter"),
+    VK("https://", "vc.com/", "vk");
 
+    private String protocol;
+    private String address;
+    private String type;
+
+    AccountTypes(String protocol, String address, String type) {
+        this.protocol = protocol;
+        this.address = address;
+        this.type = type;
+    }
+
+    public String getProtocol() {
+        return protocol;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getType() {
+        return type;
+    }
 
 }
